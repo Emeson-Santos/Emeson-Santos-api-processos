c1b6ebdea0888543985241ad70543c492565e26d
esodegemeos <esodegemeos@gmail.com>
2021-09-11 13:41:26 -0300

    modificações nos arquivos

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const cors = require('cors');
 const express = require('express');
-const server = express();
+const app = express();
 const processos = require('./src/data/processos.json');
 
 //o heroku nao retorna a porta 3000 por isso faz dessa forma pra ele criar uma porta pra mim
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 // a barra é pra pegar todas as rotas
-server.get('/processos', (req, res) => {
+app.get('/processos', (req, res) => {
     return res.json(processos)
 
 })
-server.listen(port, () => {
+app.listen(port, () => {
     console.log('servidor está funcionando...');
 });
